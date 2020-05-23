7d8fb310dc1df50aa24433496a960257075ad51c
Joaquin Peraza <joaquin@peraza.uy>
2020-05-22 21:52:34 -0300

    Update actuators.py

--- actuators.py
+++ actuators.py
@@ -56,7 +56,7 @@ class ActuadoresAgropilot:
     def crear_giro(self,step,direccion):
         self.client.set('step',str(step))
         self.client.set('dir',str(direccion))
-        print("GIRO CREADO¨)
+        print("GIRO CREADO")
 
     def runner_child(self):
         while True:
