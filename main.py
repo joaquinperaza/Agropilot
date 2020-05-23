7366f792adcd57457ef6ddd20d9cca5fcac40ece
Joaquin Peraza <joaquin@peraza.uy>
2020-05-22 21:11:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,61 +22,64 @@ limite=[]
 route=[]
 target, a, b = None, None, None
 while True:
-    while mode=="STOP":#FRENAR
-        print("stopped")
-        sleep(1)
-        mode=gps.net.get_mode()
-    while mode=="APAGAR":#APAGAR
-        sleep(1)
-        mode=gps.net.get_mode()
-    while mode=="MANUAL":
-        step,direccion=gps.net.get_test()
-        print("ORDENES DE GIRO:",step,direccion)
-        control.crear_giro(step,direccion)
-        mode=gps.net.get_mode()
-        sleep(1)
-    if mode=="TARGET":
-        target=utils.to_utm(gps.net.get_target())
-        tractor=navigator.Tractor()
-        while mode=="TARGET":
-            bearing=gps.nav
-            target_course=utils.bearing(utils.to_utm(gps.pos()),target)
-            dif=bearing-target
-            r=gps.spd/gps.rad
-            angulo=(1/r)*200
-            print("angulo:",angulo)
-            print("dif:",dif)
-            control.crear_giro(tractor.doblar(dif,angulo))
-            mode=gps.net.get_mode()
-    if mode=="GRABAR LIMITE":
-        limite=[]
-        gps.net.clear_mission()
-        while mode=="GRABAR LIMITE":
-            c=gps.get_pos()
-            limite.append(c)
-            gps.net.add_limit(c)
-            mode=gps.net.get_mode()
-            sleep(1)
-    if mode=="GRABAR A":
-        a=gps.pos()
-        gps.net.set_a(a)
-        mode=gps.net.get_mode()
-        sleep(1)
-    if mode=="GRABAR B":
-        b=gps.pos()
-        gps.net.set_b(b)
-        mode=gps.net.get_mode()
-        sleep(1)
-    if mode=="CREAR RUTA":
-        sleep(1)
-        mode=gps.net.set_mode("STOP")
-        gps.net.clear_mission()
-        route=nav_utils.create_path(a, b, limite,gps.net.get_ancho(),reverse=True,dir=0)
-        gps.net.set_wp(route)
-        mode=gps.net.get_mode()
-        sleep(1)
-    sleep(1)
-    mode=gps.net.get_mode()
+	try:
+		while mode=="STOP":#FRENAR
+			print("stopped")
+			sleep(1)
+			mode=gps.net.get_mode()
+		while mode=="APAGAR":#APAGAR
+			sleep(1)
+			mode=gps.net.get_mode()
+		while mode=="MANUAL":
+			step,direccion=gps.net.get_test()
+			print("ORDENES DE GIRO:",step,direccion)
+			control.crear_giro(step,direccion)
+			mode=gps.net.get_mode()
+			sleep(1)
+		if mode=="TARGET":
+			target=utils.to_utm(gps.net.get_target())
+			tractor=navigator.Tractor()
+			while mode=="TARGET":
+				bearing=gps.nav
+				target_course=utils.bearing(utils.to_utm(gps.pos()),target)
+				dif=bearing-target
+				r=gps.spd/gps.rad
+				angulo=(1/r)*200
+				print("angulo:",angulo)
+				print("dif:",dif)
+				control.crear_giro(tractor.doblar(dif,angulo))
+				mode=gps.net.get_mode()
+		if mode=="GRABAR LIMITE":
+			limite=[]
+			gps.net.clear_mission()
+			while mode=="GRABAR LIMITE":
+				c=gps.get_pos()
+				limite.append(c)
+				gps.net.add_limit(c)
+				mode=gps.net.get_mode()
+				sleep(1)
+		if mode=="GRABAR A":
+			a=gps.pos()
+			gps.net.set_a(a)
+			mode=gps.net.get_mode()
+			sleep(1)
+		if mode=="GRABAR B":
+			b=gps.pos()
+			gps.net.set_b(b)
+			mode=gps.net.get_mode()
+			sleep(1)
+		if mode=="CREAR RUTA":
+			sleep(1)
+			mode=gps.net.set_mode("STOP")
+			gps.net.clear_mission()
+			route=nav_utils.create_path(a, b, limite,gps.net.get_ancho(),reverse=True,dir=0)
+			gps.net.set_wp(route)
+			mode=gps.net.get_mode()
+			sleep(1)
+		sleep(1)
+		mode=gps.net.get_mode()
+	except Exception as e:
+		print("MASTER ERR", repr(e))
 
 
 
