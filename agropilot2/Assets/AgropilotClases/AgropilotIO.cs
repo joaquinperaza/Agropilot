ddd0607c9ea45745db9269748dc51c2f1e5bee63
joaquinperaza <joaquin@peraza.uy>
2021-01-20 23:57:07 -0300

    UDP + TCP Super Reliable
    UDP IO reconenct, restart after settings modification and recover.
    TCP GPS reconenct, restart after settings modification and recover.
    Nice "LED's" Showing connection flow

--- agropilot2/Assets/AgropilotClases/AgropilotIO.cs
+++ agropilot2/Assets/AgropilotClases/AgropilotIO.cs
@@ -8,55 +8,85 @@ using System.Threading;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
-
+using UnityEngine.UI;
 public class AgropilotIO : MonoBehaviour
 {
-    private TractorStatus tractorStatus;
-    public NetworkSettings networkSettings; //NEED IN DATAHUB FOR CONNECTING TO GPS
-    private TractorSettings tractorSettings;
-    private int port = 1892;
-    private IPAddress group_address = IPAddress.Parse("224.5.6.7");
+    public NetworkSettings networkSettings;
+
+    Thread UDPThread;
+    private int port;
+    private IPAddress group_address;
     IPEndPoint ip,ipGroup;
     private UdpClient client;
     private byte[] sendQueue;
+    private String IPWatchdog;
+    private int exceptionCounter=0;
+    private bool run = true;
+
+
+    [SerializeField] private Image rxLed;
+    [SerializeField] private Image txLed;
+    private bool txLedOn=false;
+    private bool rxLedOn=false;
     void Start()
     {
-        tractorStatus = GetComponent<TractorStatus>();
         networkSettings = GetComponent<NetworkSettings>();
-        tractorSettings = GetComponent<TractorSettings>();
+        IPWatchdog = networkSettings.moduleIp;
         sendQueue = new byte[0];
+        
 
-        Thread thread = new Thread(startUDP);
-        thread.Priority = System.Threading.ThreadPriority.Lowest;
-        thread.IsBackground = true;
-        Debug.Log("UDP STARTED");
-        thread.Start();
-        Debug.Log("UDP STARTED");
-
-
+        UDPThread = new Thread(startUDP);
+        UDPThread.Priority = System.Threading.ThreadPriority.Lowest;
+        UDPThread.IsBackground = true;
+        Debug.Log("UDP Starting");
+        UDPThread.Start();
+        Debug.Log("UDP Started");
+        
     }
 
     void startUDP()
     {
-        ip = new IPEndPoint(IPAddress.Any, port);
-        ipGroup = new IPEndPoint(group_address, port);
-
-        client = new UdpClient(ip);
-        client.JoinMulticastGroup(group_address);
-        client.BeginReceive(new AsyncCallback(ReceiveServerInfo), null);
-
-        while (true)
+        run = true;
+        try
         {
-            if (sendQueue.Length > 0) {
-                client.Send(sendQueue, sendQueue.Length, ipGroup);
-                Debug.Log("TX: " + String.Join(" ", sendQueue));
-                sendQueue = new byte[0];
-            }
-            Thread.Sleep(5);
-        }
-    }
+            string[] moduleIP = networkSettings.moduleIp.Split(':');
+            group_address = IPAddress.Parse(moduleIP[0]);
+            port = int.Parse(moduleIP[1]);
+            ip = new IPEndPoint(IPAddress.Any, port);
+            ipGroup = new IPEndPoint(group_address, port);
 
- 
+            client = new UdpClient(ip);
+            client.JoinMulticastGroup(group_address);
+            client.BeginReceive(new AsyncCallback(ReceiveServerInfo), null);
+        }
+        catch (Exception e)
+        {
+            RestartUDP(e);
+        }
+
+        while (run)
+        {
+            try
+            {
+                if (sendQueue.Length > 0)
+                {
+                    client.Send(sendQueue, sendQueue.Length, ipGroup);
+                    Debug.Log("TX: " + String.Join(" ", sendQueue));
+                    sendQueue = new byte[0];
+                }
+                Thread.Sleep(5);
+            }
+            catch (Exception e)
+            {
+                run = false;
+                RestartUDP(e);
+            }
+            
+        }
+
+        client.Close();
+        client.Dispose();
+    }
 
     void ReceiveServerInfo(IAsyncResult result)
 
@@ -64,26 +94,96 @@ public class AgropilotIO : MonoBehaviour
         byte[] receivedBytes = client.EndReceive(result, ref ip);
         if (receivedBytes.Length > 0)
         {
+
+            rxLedOn = true;
             Debug.Log("RX: " + String.Join(" ", receivedBytes));
+            exceptionCounter = 0;
         }
         else
         {
             Debug.Log("No data received");
         }
-        client.BeginReceive(new AsyncCallback(ReceiveServerInfo), null);
+        if (run) { client.BeginReceive(new AsyncCallback(ReceiveServerInfo), null); }
+        else {
+            client.Close();
+            client.Dispose();
+        } 
+        
 
 
     }
-
 
 
     public void Send(byte[] packet) {
+        txLedOn = true;
         IEnumerable<byte> newBuffer = sendQueue.Concat(packet);
         sendQueue = newBuffer.ToArray();
     }
+
+    private void RestartUDP(Exception e)
+    {
+        run = false;
+        Debug.Log("Restarting UDP: "+ e);
+        exceptionCounter++;
+        Thread.Sleep(500+(1000*exceptionCounter));
+
+        try {
+            startUDP();
+        }
+        catch (Exception e2)
+        {
+            Debug.Log("Restart Failed");
+            exceptionCounter++;
+            Thread.Sleep(2000 + (1000 * exceptionCounter));
+            RestartUDP(e2);
+        }
+        
+
+
+    }
+    void LedOff()
+    {
+        rxLedOn = false;
+        txLedOn = false;
+    }
     void OnApplicationQuit()
     {
-        client.Dispose();
+        run = false; //fix ghost thread running and address in use errors.
+    }
+    private void Update()
+    {
+        if (txLedOn)
+        {
+            txLed.color = Color.red;
+            Invoke("LedOff", 0.2f);
+
+        }
+        else
+        {
+            txLed.color = Color.gray;
+        }
+        if (rxLedOn)
+        {
+            rxLed.color = Color.red;
+            Invoke("LedOff", 0.2f);
+
+        }
+        else
+        {
+            rxLed.color = Color.gray;
+        }
+        if (networkSettings.moduleIp != IPWatchdog)
+        {
+            IPWatchdog = networkSettings.moduleIp;
+            run = false;
+            UDPThread = new Thread(startUDP);
+            UDPThread.Priority = System.Threading.ThreadPriority.Lowest;
+            UDPThread.IsBackground = true;
+            Debug.Log("UDP Refreshing");
+            UDPThread.Start();
+            Debug.Log("UDP Refreshed");
+
+        };
     }
 
     public AgropilotIO()
---
+++ agropilot2/Assets/AgropilotClases/GpsIO.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using SimpleTCP;
+using UnityEngine.UI;
+using System.Threading;
+
+public class GpsIO : MonoBehaviour
+{
+    private SimpleTcpClient gpsClient;
+    [SerializeField] private Image gpsLed;
+    private bool gpsLedOn = false;
+    private AgropilotIO api;
+    private String IPWatchdog;
+    private int gpsExceptionCounter = 1;
+    private TractorStatus tractorStatus;
+    public NetworkSettings networkSettings; 
+    private TractorSettings tractorSettings;
+    private DateTime last;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        api = GetComponent<AgropilotIO>();
+        tractorStatus = GetComponent<TractorStatus>();
+        networkSettings = GetComponent<NetworkSettings>();
+        tractorSettings = GetComponent<TractorSettings>();
+        IPWatchdog = null;
+        Connect(); 
+    }
+
+    // Update is called once per frame
+    void gpsLedOff() {
+        gpsLedOn = false;
+    }
+    void Update()
+    {
+        if (networkSettings.gpsIp!= IPWatchdog) {
+            IPWatchdog = networkSettings.gpsIp;
+            Connect();
+        };
+        if (gpsLedOn)
+        {
+            gpsLed.color = Color.red;
+            Invoke("gpsLedOff", 0.5f);
+        }
+        else
+        {
+            gpsLed.color = Color.gray;
+        }
+        if ((DateTime.Now - last).TotalSeconds>10) {
+            Debug.Log("GPS is offline, reconnecting");
+            gpsExceptionCounter++;
+            gpsClient.Disconnect();
+            gpsClient.Dispose();
+            last = DateTime.Now;
+            Invoke("Connect", 1f * gpsExceptionCounter);
+
+        }
+    }
+    public void Connect()
+    {
+        last = DateTime.Now;
+        try
+        {
+            gpsClient.Disconnect();
+            gpsClient.Dispose();
+        }
+        catch { }
+        try {
+            string[] ip = networkSettings.gpsIp.Split(':');
+            gpsClient = new SimpleTcpClient().Connect(ip[0], int.Parse(ip[1]));
+            gpsClient.Delimiter = 0x13;
+            gpsClient.DataReceived += (sender, msg) => {
+                last = DateTime.Now;
+                gpsLedOn = true;
+                Debug.Log("GPS NMEA: " + msg.MessageString);
+                gpsExceptionCounter = 0;
+                api.Send(msg.Data);
+            };
+
+        }
+        catch (Exception e){
+            gpsExceptionCounter++;
+            Debug.Log(e);
+            Invoke("Connect", 1f * gpsExceptionCounter);
+            try
+            {
+                gpsClient.Disconnect();
+                gpsClient.Dispose();
+            }
+            catch { }
+            
+            
+        }
+        
+
+    }
+    void OnApplicationQuit()
+    {
+        gpsClient.Disconnect();
+        gpsClient.Dispose();
+    }
+}
